d6e76d4cf101f2775cd1e41c1b3ef1c92d0fd9e9
HongBeom Choi <credtiger96@gmail.com>
2019-09-20 01:44:24 +0900

    SFTP code which can get remote csv file from server implemented with jsch library.

--- app/src/test/java/kr/ac/ajou/credtiger96/svar_a/ExampleUnitTest.kt
+++ app/src/test/java/kr/ac/ajou/credtiger96/svar_a/ExampleUnitTest.kt
@@ -2,7 +2,14 @@ package kr.ac.ajou.credtiger96.svar_a
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import android.R.attr.port
+import android.R.attr.host
+import com.jcraft.jsch.JSch
+import java.lang.Exception
+import com.jcraft.jsch.ChannelSftp
+
+
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +17,33 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private fun getFileFromServer(path : String){
+        try {
+            val jsch = JSch()
+            val session = jsch.getSession("hongbeom", "202.30.29.168", 22)
+            session.setPassword("170905")
+            val config = java.util.Properties()
+            config["StrictHostKeyChecking"] = "no"
+            session.setConfig(config)
+
+            session.connect()
+
+            val channel = session.openChannel("sftp")
+            channel.connect()
+            val channelSftp = channel as ChannelSftp
+
+            val inputStreamReader = channelSftp.get(path).reader()
+            inputStreamReader.forEachLine {
+                println(it)
+            }
+
+        }catch (e : Exception){
+            println(e.printStackTrace())
+        }
+
+    }
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        getFileFromServer("/home/hongbeom/Workspace/sensorVisualizer/log.txt")
     }
 }
